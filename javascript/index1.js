4c5c718a1c55f720fea96e8d7f7e1ec2fca71e9d
Sandeep Gupta <guptasg300@gmail.com>
2021-10-06 03:44:09 +0530

    color and background change

--- javascript/index1.js
+++ javascript/index1.js
@@ -1,5 +1,6 @@
 var count=1;
 function day(){
+    clearInterval();
    var moon = document.getElementById("moon");
    var container = document.getElementById("container");
 
@@ -8,28 +9,81 @@ function day(){
           if(moon.offsetLeft>window.innerWidth){
               clearInterval(i);
           }
-       }, 1);
-
-    setTimeout(() => {
-        if(count%2!==0){
-            moon.style.backgroundImage = "url(photos/cloud2.png)";
-            container.style.backgroundImage="url(photos/snowBackground.jpg)"
-            count++;
-        }
-        else{
-            moon.style.backgroundImage = "url(photos/moon.png)";
-            container.style.backgroundImage="url(photos/cristmas.jpg)"
-            count++;
-        }
-        
+       },  10);
+  
         var j = setInterval(() => {
            moon.style.left = (moon.offsetLeft -10)+"px";
            if(moon.offsetLeft<(window.innerWidth-350)){
-               clearInterval(j);
+            clearInterval(j);
+
+                if(count%2===0){
+                    moon.style.backgroundImage = "url(../photos/moon.png)";
+                    container.style.backgroundImage="url(../photos/wallpaperflare.com_wallpaper.jpg)";
+                    count++;
+                    animate();
+
+
+                }
+                else{
+                    moon.style.backgroundImage = "url(../photos/moon.png)";
+                    container.style.backgroundImage="url(../photos/colorNight.jpg)";
+                    count++;
+                    celebrate();
+
+                }
+
            }
           
-       }, 1);
-    }, 500);
+       }, 20);
+
+    
+function celebrate(){
+    clearInterval(s);
+    var snows = document.getElementsByClassName("snow");
+    var container = document.getElementById("container");
+    var moon = document.getElementById("moon");
+
+    
+   
+    for (const snow of snows) {
+        snow.style.filter = "blur(0px)";
+
+        snow.style.left=(Math.random()*window.innerWidth) +"px";
+        snow.style.backgroundColor="rgba("
+        +Math.random()*255+
+        ","
+        +Math.random()*255+
+        ","
+        +Math.random()*255+
+        ","
+        +1+
+        ")";
+
+        snow.style.width = "20px";
+        snow.style.height = "20px";
+        let pos = Math.floor(Math.random()*700);
+        if(pos%2==0){
+            snow.style.top = pos+"px";
+        }else{
+            snow.style.top = "-"+pos+"px";
+        }
+    }
+
+    var s = setInterval(() => {
+
+        for (const snow of snows) {
+            snow.style.top = snow.offsetTop +1 +"px";
+        
+            if(snow.offsetTop>window.innerHeight){
+                snow.style.top="0px";
+            }
+        }
+        
+    }, 200);
+
+}
+
+
 
 
 }
--- javascript/index2.js
+++ javascript/index2.js
@@ -1,20 +1,14 @@
 function animate(){
+    clearInterval(snowfall);
    
     var snows = document.getElementsByClassName("snow");
-    var santa = document.getElementById("santa");
-    var container = document.getElementById("container");
-    var cloud = document.getElementById("cloud");
-    var p = document.getElementsByTagName("p")[0];
-    
  
-
     for (const snow of snows) {
+        snow.style.backgroundColor="white";
         snow.style.left=(Math.random()*window.innerWidth) +"px";
         var a =Math.random()*20;
         snow.style.width = a+"px";
         snow.style.height = a+"px";
-        // snow.style.width="2px";
-        // snow.style.height="10px";
         let pos = Math.floor(Math.random()*700);
         if(pos%2==0){
             snow.style.top = pos+"px";
@@ -25,40 +19,12 @@ function animate(){
        
 
     }
-//     var images = ["url(/photos/santa1.png)",
-//                     "url(/photos/santa2.png)",
-//                     "url(/photos/santa3.png)",
-//                 "url(/photos/santa4.png)",
-//             "url(/photos/santa5.png)",
-//         "url(/photos/santa6.png)",
-//     "url(/photos/santa7.png)",
-// "url(/photos/santa8.png)",
-// "url(/photos/santa9.jpg)",
-// "url(/photos/santa10.jpg)",
-// "url(/photos/santa11.jpg)",
-// "url(/photos/santa12.jpg)",
-// "url(/photos/santa13.jpg)",
-// "url(/photos/santa14.jpg)",
-// "url(/photos/santa15.jpg)",
-// "url(/photos/santa16.jpg)"];
 
-    // var i=0,j=0;
-   var s = setInterval(() => {
 
-        // cloud.style.backgroundPositionX=i+"px";
-        // i--;
-        // santa.style.backgroundImage=images[j];
-        // santa.style.left=i+"px";
-        // i+=8;
-        // j++;
-        // if(j>15){
-        //     j=0;
-        // }
-        // if(i>1500){
-        //     i=0;
-        // }
+   var snowfall = setInterval(() => {
+
         for (const snow of snows) {
-            snow.style.top = snow.offsetTop +Math.floor(Math.random()+3) +"px";
+            snow.style.top = snow.offsetTop +2 +"px";
             if(snow.offsetTop>window.innerHeight){
                 snow.style.top="0px";
             }
